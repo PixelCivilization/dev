543f54ca781b1cfec5dfd5338b62c889141c80e2
Karol <karold636@wp.pl>
2016-07-14 15:08:51 +0000

    Alpha 0.2 (Save&Load)

--- game.js
+++
@@ -1,9 +0,0 @@
-var game = new Phaser.Game(1000, 1000, Phaser.CANVAS, 'myMainProject-dev'); 
-
-
-game.state.add('boot', bootState);
-game.state.add('load', loadState);
-game.state.add('mainMenu', mainMenuState);
-game.state.add('mainState', mainState);
-
-game.state.start('boot');
--- subsystems/tech.js
+++
@@ -1,6 +0,0 @@
-function unlockHousingAndWorkers() {
-    techUnlockHousing = true;
-    techUnlockWorkers = true;
-    writeLog("Workers has been discovered!",2);
-    writeLog("Housing has been discovered!",2);
-}
